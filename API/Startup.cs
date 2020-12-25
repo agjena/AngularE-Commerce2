bc9b0ad75fe0843d40bbec85017dad1a42c7bb75
Ali Göksu <ali.goksu1@outlook.com>
2020-12-25 19:12:48 +0300

    Proje mimari altyapısı kuruldu

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using API.Data.DataContext;
+using API.Infrastructure.DataContext;
 using Microsoft.EntityFrameworkCore;
 
 namespace API
